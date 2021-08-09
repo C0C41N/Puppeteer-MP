705bb06ab0df6edecbb6d736e60ba8d8c8c744c9
C0C41N <0ct41n@gmail.com>
2021-08-09 20:27:07 +0500

    dlTracks

--- src/services/dlTracks.ts
+++ src/services/dlTracks.ts
@@ -1,3 +1,4 @@
+import { MultiBar, Presets } from 'cli-progress';
 import { createWriteStream, readFileSync } from 'fs';
 import * as ytdl from 'ytdl-core';
 
@@ -5,14 +6,26 @@ import { Tracks } from '../common/types';
 import { getResourcesFolder, getTracksFilePath, log } from '../common/util';
 
 import type { downloadOptions } from 'ytdl-core';
-
 export const dlTracks = async () => {
-	// Reading file
+	// reading file
 
 	const tracksFile = getTracksFilePath();
 	const data = readFileSync(tracksFile, { encoding: 'utf8' });
 
 	const tracks: Tracks = JSON.parse(data.trim());
+
+	// setup progress bars
+
+	const multibar = new MultiBar(
+		{
+			clearOnComplete: true,
+			hideCursor: true,
+			format: '{bar} {percentage}% | {track}',
+			fps: 60,
+			autopadding: true,
+		},
+		Presets.shades_classic
+	);
 
 	// ytdl-core
 
@@ -25,15 +38,23 @@ export const dlTracks = async () => {
 				quality: 'highestaudio',
 			};
 
+			const bar = multibar.create(100, 0);
+
 			const stream = ytdl(url, options);
 
 			const file = `${getResourcesFolder()}/${title}`;
 
 			stream.pipe(createWriteStream(file));
 
+			stream.on('progress', (_, downloaded: number, total: number) => {
+				const percent = (downloaded * 90) / total;
+				bar.update(percent);
+			});
+
 			return new Promise<void>(resolve => {
 				stream.once('end', () => {
 					log(`${title} | Done`);
+					multibar.stop();
 					resolve();
 				});
 			});
