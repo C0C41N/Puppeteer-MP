e09bd9fc7767f35ef3c4ebe33cb8af66494df904
C0C41N <0ct41n@gmail.com>
2021-08-09 07:16:39 +0500

    ok

--- src/services/listing.ts
+++ src/services/listing.ts
@@ -1,4 +1,4 @@
-import { existsSync } from 'fs';
+import { existsSync, mkdirSync } from 'fs';
 import { resolve } from 'path';
 import * as puppeteer from 'puppeteer';
 
@@ -38,107 +38,122 @@ export const listing = async () => {
 
 	console.log('| adding details');
 
-	for (const e of editPages) {
+	const titles = await Promise.all(
+		editPages.map(async e => {
+			// clear SKU
+
+			await e.waitForSelector('#product_sku');
+			await e.$eval('#product_sku', (e: HTMLInputElement) => (e.value = ''));
+
+			// check Active
+
+			await e.click('#product_item_status_active');
+
+			// get Title of item
+
+			const title = await e.$eval(
+				'#product_en-US_name',
+				(e: HTMLInputElement) => e.value
+			);
+
+			// put description
+
+			await e.$eval(
+				'#product_en-US_description',
+				(e: HTMLTextAreaElement, title: string) => (e.value = title),
+				title
+			);
+
+			// put keywords
+
+			const keywords = title.split(' ').join(', ') + ", feat, ft., ft, didn't";
+
+			await e.$eval(
+				'#product_en-US_keywords',
+				(e: HTMLTextAreaElement, keywords: string) => (e.value = keywords),
+				keywords
+			);
+
+			// upload image
+
+			await e.click('#upload-image-link');
+
+			await e.waitForSelector('#image-upload-button');
+
+			return title;
+		})
+	);
+
+	for (const [i, e] of editPages.entries()) {
 		await e.bringToFront();
-
-		// clear SKU
-
-		await e.waitForSelector('#product_sku');
-		await e.$eval('#product_sku', (e: HTMLInputElement) => (e.value = ''));
-
-		// check Active
-
-		await e.click('#product_item_status_active');
-
-		// get Title of item
-
-		const title = await e.$eval(
-			'#product_en-US_name',
-			(e: HTMLInputElement) => e.value
-		);
-
-		// put description
-
-		await e.$eval(
-			'#product_en-US_description',
-			(e: HTMLTextAreaElement, title: string) => (e.value = title),
-			title
-		);
-
-		// put keywords
-
-		const keywords = title.split(' ').join(', ') + ", feat, ft., ft, didn't";
-
-		await e.$eval(
-			'#product_en-US_keywords',
-			(e: HTMLTextAreaElement, keywords: string) => (e.value = keywords),
-			keywords
-		);
-
-		// upload image
-
-		await e.click('#upload-image-link');
-
-		await e.waitForSelector('#image-upload-button');
 
 		await e.hover('#image-upload-button');
 
 		await e.waitForSelector('input[name="image[attachment]"]');
-
-		const uploadEl = await e.$('input[name="image[attachment]"]');
-
-		const tmpFilePath = resolve(`resources/${title}`);
-		const jpeg = tmpFilePath + '.jpg';
-		const png = tmpFilePath + '.png';
-
-		const filePath = existsSync(jpeg) ? jpeg : existsSync(png) ? png : null;
-
-		if (!filePath) throw Error("Can't find file to upload");
-
-		const imagesChanged = () =>
-			new Promise<void>(async resolve => {
-				const sel = '#image-list';
-
-				const count = await numChildren(e, sel);
-
-				const interval = setInterval(async () => {
-					const xount = await numChildren(e, sel);
-					if (count < xount) {
-						clearInterval(interval);
-						resolve();
-					}
-				}, 250);
-			});
-
-		console.log('| uploading file');
-
-		await Promise.all([imagesChanged(), uploadEl.uploadFile(filePath)]);
-
-		console.log('| upload done');
-
-		const submit =
-			'.form-buttons > button[class="button cart-button"][type="submit"]';
-
-		console.log('| submitting');
-
-		const clicker = () =>
-			new Promise<void>(resolve => {
-				const interval = setInterval(async () => {
-					try {
-						await e.click(submit);
-						resolve();
-					} catch (error) {
-						clearInterval(interval);
-					}
-				}, 500);
-			});
-
-		await Promise.all([e.waitForNavigation(), clicker()]);
-
-		e.close();
-
-		console.log(`\n| ${title} // done\n`);
 	}
+
+	await Promise.all(
+		editPages.map(async (e, i) => {
+			const uploadEl = await e.$('input[name="image[attachment]"]');
+
+			const folder = resolve(`resources/${titles[i]}`);
+			!existsSync(folder) && mkdirSync(folder);
+
+			const filePath = [0, 1]
+				.map(x => {
+					const jpeg = `${folder}/${x}.jpg`;
+					const png = `${folder}/${x}.png`;
+					return existsSync(jpeg) ? jpeg : existsSync(png) ? png : null;
+				})
+				.filter(e => e !== null)[0];
+
+			if (!filePath) throw Error("Can't find file to upload");
+
+			const imagesChanged = () =>
+				new Promise<void>(async resolve => {
+					const sel = '#image-list';
+
+					const count = await numChildren(e, sel);
+
+					const interval = setInterval(async () => {
+						const xount = await numChildren(e, sel);
+						if (count < xount) {
+							clearInterval(interval);
+							resolve();
+						}
+					}, 250);
+				});
+
+			console.log('| uploading file');
+
+			await Promise.all([imagesChanged(), uploadEl.uploadFile(filePath)]);
+
+			console.log('| upload done');
+
+			const submit =
+				'.form-buttons > button[class="button cart-button"][type="submit"]';
+
+			console.log('| submitting');
+
+			const clicker = () =>
+				new Promise<void>(resolve => {
+					const interval = setInterval(async () => {
+						try {
+							await e.click(submit);
+							resolve();
+						} catch (error) {
+							clearInterval(interval);
+						}
+					}, 500);
+				});
+
+			await Promise.all([e.waitForNavigation(), clicker()]);
+
+			e.close();
+
+			console.log(`\n| ${titles[i]} // done\n`);
+		})
+	);
 
 	console.log('\n| All Done\n');
 };
