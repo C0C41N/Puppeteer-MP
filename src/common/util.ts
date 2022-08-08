8c518e39498257d81b19010c6c75418d79e58390
C0C41N <0ct41n@gmail.com>
2022-08-08 12:02:26 +0500

    added MacOS support

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,78 +1,78 @@
-import { closeSync, existsSync, mkdirSync, openSync } from 'fs';
-import { resolve } from 'path';
-import { Page } from 'puppeteer';
+import { closeSync, existsSync, mkdirSync, openSync } from 'fs'
+import { resolve } from 'path'
+import { Page } from 'puppeteer'
 
-import { thumbsFileName, titlesFileName, tracksFileName } from './const';
-import { logOptions } from './types';
+import { thumbsFileName, titlesFileName, tracksFileName } from './const'
+import { logOptions } from './types'
 
 export const log = (text: string, options?: logOptions) => {
-	const o = options || { cls: false, decor: true, preLines: 0 };
-	const { cls, decor, preLines } = o;
+	const o = options || { cls: false, decor: true, preLines: 0 }
+	const { cls, decor, preLines } = o
 
-	if (!!cls) console.clear();
+	if (!!cls) console.clear()
 
 	const lines = Array(preLines || 0)
 		.fill('\n')
-		.join('');
+		.join('')
 
-	const decorT = !!decor ? '| ' : '';
+	const decorT = !!decor ? '| ' : ''
 
-	console.log(`${lines}${decorT}${text}`);
-};
+	console.log(`${lines}${decorT}${text}`)
+}
 
 export const prompt = async (text: string) =>
 	new Promise<string>(resolve => {
-		process.stdout.write(text);
-		process.stdin.read();
+		process.stdout.write(text)
+		process.stdin.read()
 		process.stdin.once('data', data => {
-			const filteredData = data.toString().split('\r\n').join('');
-			resolve(filteredData);
-		});
-	});
+			const filteredData = data.toString().split(/\r?\n/).join('')
+			resolve(filteredData)
+		})
+	})
 
 export const numChildren = async (page: Page, sel: string) =>
-	page.$eval(sel, e => e.childElementCount);
+	page.$eval(sel, e => e.childElementCount)
 
 // Folders and paths
 
 export const getTitleFolder = (title: string) => {
-	const folder = `${getThumbsFolder()}/${title}`;
-	!existsSync(folder) && mkdirSync(folder);
-	return folder;
-};
+	const folder = `${getThumbsFolder()}/${title}`
+	!existsSync(folder) && mkdirSync(folder)
+	return folder
+}
 
 export const getThumbsFolder = () => {
-	const folder = `${getResourcesFolder()}/thumbs`;
-	!existsSync(folder) && mkdirSync(folder);
-	return folder;
-};
+	const folder = `${getResourcesFolder()}/thumbs`
+	!existsSync(folder) && mkdirSync(folder)
+	return folder
+}
 
 export const getTracksFolder = () => {
-	const folder = `${getResourcesFolder()}/tracks`;
-	!existsSync(folder) && mkdirSync(folder);
-	return folder;
-};
+	const folder = `${getResourcesFolder()}/tracks`
+	!existsSync(folder) && mkdirSync(folder)
+	return folder
+}
 
 export const getResourcesFolder = () => {
-	const folder = resolve('resources');
-	!existsSync(folder) && mkdirSync(folder);
-	return folder;
-};
+	const folder = resolve('resources')
+	!existsSync(folder) && mkdirSync(folder)
+	return folder
+}
 
 export const getTitlesFilePath = () => {
-	const file = `${getResourcesFolder()}/${titlesFileName}`;
-	!existsSync(file) && closeSync(openSync(file, 'w'));
-	return file;
-};
+	const file = `${getResourcesFolder()}/${titlesFileName}`
+	!existsSync(file) && closeSync(openSync(file, 'w'))
+	return file
+}
 
 export const getTracksFilePath = () => {
-	const file = `${getResourcesFolder()}/${tracksFileName}`;
-	!existsSync(file) && closeSync(openSync(file, 'w'));
-	return file;
-};
+	const file = `${getResourcesFolder()}/${tracksFileName}`
+	!existsSync(file) && closeSync(openSync(file, 'w'))
+	return file
+}
 
 export const getThumbsFilePath = () => {
-	const file = `${getResourcesFolder()}/${thumbsFileName}`;
-	!existsSync(file) && closeSync(openSync(file, 'w'));
-	return file;
-};
+	const file = `${getResourcesFolder()}/${thumbsFileName}`
+	!existsSync(file) && closeSync(openSync(file, 'w'))
+	return file
+}
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,49 @@
-import { log, prompt } from './common/util';
-import { dlThumbs } from './services/dlThumbs';
-import { dlTracks } from './services/dlTracks';
-import { fetchTitles } from './services/fetchTitles';
-import { listing } from './services/listing';
+import { log, prompt } from './common/util'
+import { dlThumbs } from './services/dlThumbs'
+import { dlTracks } from './services/dlTracks'
+import { fetchTitles } from './services/fetchTitles'
+import { listing } from './services/listing'
 
 const init = async () => {
+	log('', { cls: true, decor: false })
+
 	const text =
 		'\n\n1 - Fetch titles\n\n2 - Download thumbs' +
-		'\n\n3 - List\n\n4 - Dowanload tracks\n\nEnter\t';
+		'\n\n3 - List\n\n4 - Dowanload tracks\n\nEnter\t'
 
-	const choice = await prompt(text);
+	const choice = await prompt(text)
 
-	log('', { cls: true, decor: false, preLines: 2 });
+	log('', { cls: true, decor: false })
 
 	switch (choice) {
 		case '1':
-			await fetchTitles();
-			await init();
-			break;
+			await fetchTitles()
+			await init()
+			break
 
 		case '2':
-			await dlThumbs();
-			await init();
-			break;
+			await dlThumbs()
+			await init()
+			break
 
 		case '3':
-			await listing();
-			await init();
-			break;
+			await listing()
+			await init()
+			break
 
 		case '4':
-			await dlTracks();
-			await init();
-			break;
+			await dlTracks()
+			await init()
+			break
 
 		case 'all':
-			await fetchTitles();
-			await dlThumbs();
-			await listing();
-			break;
+			await fetchTitles()
+			await dlThumbs()
+			await listing()
+			break
 	}
 
-	process.exit(0);
-};
+	process.exit(0)
+}
 
-init();
+init()
